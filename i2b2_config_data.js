2bb697c050e863fc16126267110957eb038bba80
Dileep Gunda <dgunda.gsu@gmail.com>
2020-02-19 12:12:07 -0500

    all changes into single commit

--- i2b2_config_data.js
+++ i2b2_config_data.js
@@ -1,16 +1,17 @@
 {
-	urlProxy: "index.php",
-	urlFramework: "js-i2b2/",
-	startZoomed: true,
+	urlProxy: "https://localhost:8443/eurekaclinical-i2b2-integration-webapp/i2b2/",
+    urlFramework: "js-i2b2/",
+    casDomain: "i2b2demo",
 	//-------------------------------------------------------------------------------------------
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
 		{ domain: "i2b2demo",
 		  name: "HarvardDemo",
-		  urlCellPM: "http://services.i2b2.org/i2b2/services/PMService/",
+		  urlCellPM: "http://localhost:8443/i2b2/services/PMService/",
 		  allowAnalysis: true,
-		  //installer: "/webclient/plugin_installer/",
-		  debug: false
+		  debug: false,
+		  "EC_I2B2_INTEGRATION_URL": "https://localhost:8443/eurekaclinical-i2b2-integration-webapp"
+		  "CAS_SERVER": "https://localhost:8443/cas-server"
 		}
 	]
 	//-------------------------------------------------------------------------------------------
